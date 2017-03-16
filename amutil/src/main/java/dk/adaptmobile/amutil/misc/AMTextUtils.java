d10ed8406363a9b2b9fb189038d0fa57e3b36130
Bjarke Severinsen <bjarkeseverinsen@Bjarkes-MBP.lbg.adapt.dk>
2017-03-16 13:13:55 +0100

    Added AMTextUtils

---
+++ amutil/src/main/java/dk/adaptmobile/amutil/misc/AMTextUtils.java
@@ -0,0 +1,33 @@
+package dk.adaptmobile.amutil.misc;
+
+/**
+ * Created by christiansteffensen on 16/03/2017.
+ */
+public class AMTextUtils {
+    private static final String TAG = "AMTextUtils";
+    /**
+     * Returns true if the string is null or 0-length.
+     *
+     * @param str the string to be examined
+     * @return true if str is null or zero length
+     */
+    public static boolean isEmpty(CharSequence str) {
+        if (str == null || str.length() == 0)
+            return true;
+        else
+            return false;
+    }
+    /**
+     * Returns whether the given CharSequence contains only digits.
+     */
+    public static boolean isDigitsOnly(CharSequence str) {
+        final int len = str.length();
+        for (int cp, i = 0; i < len; i += Character.charCount(cp)) {
+            cp = Character.codePointAt(str, i);
+            if (!Character.isDigit(cp)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
